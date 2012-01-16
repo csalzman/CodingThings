fcf4d43d063e0f591e1b8849869cb909f857b5dd
Chris Salzman <csalzman@gmail.com>
2012-01-16 13:47:47 -0500

    Tightened some things up

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,25 +1,26 @@
 #Fibonacci's sequence
 
-puts "Displays the Fibonacci sequence up to a certain number. \n\nWhen should I stop?"
+puts "\nDisplays the Fibonacci sequence up to a certain number. When should it stop?"
 prompt = "> "
 print prompt
 entries = gets.chomp().to_i
 
 #sets first three numbers
-first = 1
+first = 0
 second = 1
-number = 0
+iterations = 1
 
 #displays beginning part of sequence
-puts number, first, second
+puts first, second
 
 #loops through rest of specified entries
 while second < entries + 1
+	iterations = iterations + 1
 	number = first + second
 	if number <= entries
 		puts number
 	else
-		puts ""
+		puts "#{iterations} numbers in the sequence."
 	end
 	first = second
 	second = number
